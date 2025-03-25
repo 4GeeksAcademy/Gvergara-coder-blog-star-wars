ad9a4985d3baee8015fd5cd9cd17b68d2445fba5
Gabriel Vergara <69174732+Gvergara-coder@users.noreply.github.com>
2025-03-25 00:58:31 +0000

    [se edito el css]

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,21 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="first-navbar navbar">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRecxSfklxII2Rdm_-e3pPj05NO3xdKgyzOM3ONKbkTeEtQJevLT045WJoX47REFCqRUSs&usqp=CAU" alt="ir al inicio" />
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+						<div className="btn-group" role="group">
+							<button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+								Favorites
+							</button>
+							<ul className="dropdown-menu">
+								<li><a className="dropdown-item" href="#">Dropdown link</a></li>
+								<li><a className="dropdown-item" href="#">Dropdown link</a></li>
+							</ul>
+						</div>
 				</div>
 			</div>
 		</nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,196 @@ import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+
+			<div className="horizontal-scroll-container">
+				<h1 className="tittle">
+					Characters
+				</h1>
+				<div className="card-header container">
+
+					<div className="card star-card" style={{ width: "18rem" }}>
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on .</p>
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">An item</li>
+							<li className="list-group-item">A second item</li>
+							<li className="list-group-item">A third item</li>
+						</ul>
+						<div className="card-body">
+							<button type="button" className="btn btn-outline-success">Learn more!</button>
+							<button type="button" className="btn btn-outline-warning"><i class="fa-solid fa-heart"></i></button>
+						</div>
+					</div>
+
+					<div className="card star-card" style={{ width: "18rem" }}>
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on the card tit.</p>
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">An item</li>
+							<li className="list-group-item">A second item</li>
+							<li className="list-group-item">A third item</li>
+						</ul>
+						<div className="card-body">
+							<button type="button" className="btn btn-outline-success">Learn more!</button>
+							<button type="button" className="btn btn-outline-warning"><i class="fa-solid fa-heart"></i></button>
+						</div>
+					</div>
+
+					<div className="card star-card" style={{ width: "18rem" }}>
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up.</p>
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">An item</li>
+							<li className="list-group-item">A second item</li>
+							<li className="list-group-item">A third item</li>
+						</ul>
+						<div className="card-body">
+							<button type="button" className="btn btn-outline-success">Learn more!</button>
+							<button type="button" className="btn btn-outline-warning"><i class="fa-solid fa-heart"></i></button>
+						</div>
+					</div>
+
+					<div className="card star-card" style={{ width: "18rem" }}>
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on the card title and m.</p>
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">An item</li>
+							<li className="list-group-item">A second item</li>
+							<li className="list-group-item">A third item</li>
+						</ul>
+						<div className="card-body">
+							<button type="button" className="btn btn-outline-success">Learn more!</button>
+							<button type="button" className="btn btn-outline-warning"><i class="fa-solid fa-heart"></i></button>
+						</div>
+					</div>
+
+					<div className="card star-card" style={{ width: "18rem" }}>
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on the card title an.</p>
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">An item</li>
+							<li className="list-group-item">A second item</li>
+							<li className="list-group-item">A third item</li>
+						</ul>
+						<div className="card-body">
+							<button type="button" className="btn btn-outline-success">Learn more!</button>
+							<button type="button" className="btn btn-outline-warning"><i class="fa-solid fa-heart"></i></button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="horizontal-scroll-container">
+				<h1 className="tittle">
+					Planets
+				</h1>
+				<div className="card-header container">
+
+					<div className="card star-card" style={{ width: "18rem" }}>
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on .</p>
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">An item</li>
+							<li className="list-group-item">A second item</li>
+							<li className="list-group-item">A third item</li>
+						</ul>
+						<div className="card-body">
+							<button type="button" className="btn btn-outline-success">Learn more!</button>
+							<button type="button" className="btn btn-outline-warning"><i class="fa-solid fa-heart"></i></button>
+						</div>
+					</div>
+
+					<div className="card star-card" style={{ width: "18rem" }}>
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on the card tit.</p>
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">An item</li>
+							<li className="list-group-item">A second item</li>
+							<li className="list-group-item">A third item</li>
+						</ul>
+						<div className="card-body">
+							<button type="button" className="btn btn-outline-success">Learn more!</button>
+							<button type="button" className="btn btn-outline-warning"><i class="fa-solid fa-heart"></i></button>
+						</div>
+					</div>
+
+					<div className="card star-card" style={{ width: "18rem" }}>
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up.</p>
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">An item</li>
+							<li className="list-group-item">A second item</li>
+							<li className="list-group-item">A third item</li>
+						</ul>
+						<div className="card-body">
+							<button type="button" className="btn btn-outline-success">Learn more!</button>
+							<button type="button" className="btn btn-outline-warning"><i class="fa-solid fa-heart"></i></button>
+						</div>
+					</div>
+
+					<div className="card star-card" style={{ width: "18rem" }}>
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on the card title and m.</p>
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">An item</li>
+							<li className="list-group-item">A second item</li>
+							<li className="list-group-item">A third item</li>
+						</ul>
+						<div className="card-body">
+							<button type="button" className="btn btn-outline-success">Learn more!</button>
+							<button type="button" className="btn btn-outline-warning"><i class="fa-solid fa-heart"></i></button>
+						</div>
+					</div>
+
+					<div className="card star-card" style={{ width: "18rem" }}>
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on the card title an.</p>
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">An item</li>
+							<li className="list-group-item">A second item</li>
+							<li className="list-group-item">A third item</li>
+						</ul>
+						<div className="card-body">
+							<button type="button" className="btn btn-outline-success">Learn more!</button>
+							<button type="button" className="btn btn-outline-warning"><i class="fa-solid fa-heart"></i></button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }; 
